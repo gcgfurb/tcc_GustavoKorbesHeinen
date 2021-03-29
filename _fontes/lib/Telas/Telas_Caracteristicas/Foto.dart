fc874d6ccec9f3dec36313e382e510d5776225da
Gustavo K Heinen <gustavok.heinen@gmail.com>
2021-03-29 20:59:13 -0300

    Implementando parte de fotos no aplicativo

--- _fontes/lib/Telas/Telas_Caracteristicas/Foto.dart
+++ _fontes/lib/Telas/Telas_Caracteristicas/Foto.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:TCC_II/Classes/Atividade.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ClasseFoto extends StatefulWidget {
   Atividade _atividade = new Atividade();
@@ -13,101 +13,116 @@ class ClasseFoto extends StatefulWidget {
 }
 
 class Foto extends State<ClasseFoto> {
-  CameraDescription firstCamera;
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
+  PickedFile imageFile;
 
-  criaObjetoCamera() async {
-    List<CameraDescription> cameras = await availableCameras();
-    firstCamera = cameras.first;
+  _openCamera(BuildContext context) async {
+    var picture = await ImagePicker.platform.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = picture;
+    });
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = CameraController(
-      firstCamera,
-      ResolutionPreset.medium,
-    );
-
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
+  Widget _decideImageView() {
+    if (imageFile == null) {
+      return Expanded(
+        child: Center(
+          child: Text("Nenhuma imagem no momento"),
+        ),
+      );
+    } else {
+      return Expanded(
+          child: Image.file(
+        File(imageFile.path),
+        width: 400,
+        height: 400,
+      ));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            // If the picture was taken, display it on a new screen.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image?.path,
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        color: Colors.green[300],
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+        child: Row(
+          children: <Widget>[
+            _decideImageView(),
+            Expanded(
+              child: IntrinsicWidth(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: TextField(
+                        //focusNode: focusNodeDescricao,
+                        //controller: descricaoTexto,
+                        maxLength: 150,
+                        maxLines: 7,
+                        decoration: InputDecoration(
+                          hintText: 'Objetivo geral da atividade de campo*',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      child: RaisedButton(
+                        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                        color: Colors.green[500],
+                        textColor: Colors.white,
+                        child: Text("Alterar imagem"),
+                        onPressed: () {
+                          _openCamera(context);
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Container(
+                            width: 150,
+                            child: RaisedButton(
+                              padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                              color: Colors.green[500],
+                              textColor: Colors.white,
+                              child: Text("Gravar"),
+                              onPressed: () {
+                                //_openCamera(context);
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: 150,
+                            child: RaisedButton(
+                              padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                              color: Colors.green[500],
+                              textColor: Colors.white,
+                              child: Text("Cancelar"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
     );
   }
 }
